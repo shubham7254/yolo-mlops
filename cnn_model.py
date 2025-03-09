36c37627dee2c0e001137ac57141063a76815dec
Shubham Jagtap <69067628+shubham7254@users.noreply.github.com>
2025-03-09 18:39:10 -0400

    Update cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -1,1 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(128 * 32 * 32, 256)
+        self.fc2 = nn.Linear(256, 10)  # 10 classes for CIFAR-10
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.adaptive_avg_pool2d(x, (32, 32))
+        x = x.view(-1, 128 * 32 * 32)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+if __name__ == "__main__":
+    model = CNNModel()
+    print(model)
 
